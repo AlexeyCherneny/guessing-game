deddc1bd518e8d4bab16f0125ec06c146ddaff62
Chirforever <a.cherneny@gmail.com>
2017-02-05 23:27:39 +0300

    Finished game!

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,32 @@
 class GuessingGame {
-    constructor() {}
+    constructor(min = 0, max = 0) {
+    	this.guessStart = min;
+    	this.guessEnd = max;
+    	this.min = min;
+    	this.max = max;
+    	this.temp = 0;
+    }
 
     setRange(min, max) {
-
+    	this.guessStart = min;
+    	this.guessEnd = max;
+    	this.min = min;
+    	this.max = max;
     }
 
     guess() {
-
+    	this.temp = Math.round((this.guessStart + this.guessEnd)/2);
+    	
+    	return this.temp;
     }
 
     lower() {
-
+    	this.guessEnd = this.temp;
+    	
     }
 
     greater() {
-
+    	this.guessStart = this.temp;
     }
 }
 
